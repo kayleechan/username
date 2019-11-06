0cd09793f87e73b3451116add2c70bf257aa085a
Kaylee Chan <kaychan@chapman.edu>
2019-11-06 10:47:49 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Kaylee Chan
 echo "Enter a username: "
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][0-9a-z_][2-11]$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][0-9a-z_]{2-11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - five digits only!"
 	echo "Enter a username: "
