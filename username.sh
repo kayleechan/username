2caa79db70d823791f4733f245dd1517f95c2954
Kaylee Chan <kaychan@chapman.edu>
2019-11-06 10:51:31 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # username.sh
 # Kaylee Chan
-echo "Enter a username: "
+echo "Enter a five-digit ZIP code: "
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][0-9a-z_]{2-11}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username - five digits only!"
-	echo "Enter a username: "
+	echo "You must enter a valid ZIP code - five digits only!"
+	echo "Enter a five-digit ZIP code: "
 	read USERNAME
 done
 echo "Thank you"
